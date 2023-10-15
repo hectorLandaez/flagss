642afe4bacfe3ec638ba2f73656d384898e9d9ba
mi hector <hectorlandaezopsu@gmail.com>
2023-10-14 19:34:49 -0500

    fetch funciona

--- flags/src/App.jsx
+++ flags/src/App.jsx
@@ -1,16 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((jsonData) => setData(jsonData))
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   return (
-    <>
-     
-    </>
-  )
+    <div>
+      <h2>Posts from API:</h2>
+      <ul>
+        {console.log(data)}
+        {data.map((countrie) => (
+          <li>
+            {countrie.name.common}
+            <img src={countrie.flags.svg} alt="" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
 
 export default App
