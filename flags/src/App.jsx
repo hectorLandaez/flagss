adf2142fd46ab92736fe1904a589e0e8a4062f9d
mi hector <hectorlandaezopsu@gmail.com>
2023-10-14 20:20:56 -0500

    primer filtro completo y segundo casi

--- flags/src/App.jsx
+++ flags/src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 function App() {
 
   const [data, setData] = useState([]);
+  const [inputValue, setInputValue] =useState([])
+  const [buttonValue, setButtonValue] =useState('')
+
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -14,15 +17,42 @@ function App() {
       });
   }, []);
 
+  const handleFilter= (event)=>{
+    event.preventDefault();
+    let pfiltrados = data.filter((pais)=>pais.name.common.includes(inputValue))
+    setData(pfiltrados)
+  }
+
+  const handleFilterRegion =(value)=>{
+    setButtonValue(value)
+    console.log(buttonValue)
+  }
   return (
     <div>
-      <h2>Posts from API:</h2>
+      <form onSubmit={handleFilter}>
+      <input type="text" 
+      value={inputValue}
+      onChange={(e)=>setInputValue(e.target.value)}
+      />
+      <button type='submit'>buscar</button>
+      </form>
+
+      <button>REGIONES</button>
+        <ul className='lregiones'>
+            <li><button onClick={()=>handleFilterRegion('oceania')}>oceania</button></li>
+            <li><button onClick={()=>handleFilterRegion('americas')}>americas</button></li>
+            <li><button onClick={()=>handleFilterRegion('africa')} >africa</button></li>
+            <li><button onClick={()=>handleFilterRegion('europe')} >europe</button></li>
+            <li><button onClick={()=>handleFilterRegion('asia')}>asia</button></li>
+            <li><button onClick={()=>handleFilterRegion('antartic')} >antartic</button></li>
+        </ul>
+    
+      <h2>countries:</h2>
       <ul>
-        {console.log(data)}
         {data.map((countrie) => (
           <li>
             {countrie.name.common}
-            <img src={countrie.flags.svg} alt="" />
+            {/* <img src={countrie.flags.svg} alt="" /> */}
           </li>
         ))}
       </ul>
